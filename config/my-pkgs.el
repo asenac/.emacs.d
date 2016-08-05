1cc54b10b0f1e1d2d3466d22e2f93e82f86dc2ac
Andres Senac <andres@senac.es>
2016-08-05 14:13:13 +0100

    Use exec-path-from-shell in linux as well

--- config/my-pkgs.el
+++ config/my-pkgs.el
@@ -4,8 +4,7 @@
 (use-package exec-path-from-shell
   :config
   (progn
-    (when (memq window-system '(mac ns))
-      (exec-path-from-shell-initialize))))
+    (exec-path-from-shell-initialize)))
 
 ;;------------------------------------------------------------------------------
 ;; linum-relative

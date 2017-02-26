05977c5e2bf24ecb08938ce39e7700a12311da98
Andres Senac <andres@senac.es>
2017-02-26 15:09:51 +0000

    Ooops, use :init instead of :config

--- config/my-csharp.el
+++ config/my-csharp.el
@@ -1,22 +1,24 @@
 (use-package omnisharp
+  :defer t
   :ensure nil
   :mode ("\\.cs\\'" . csharp-mode)
-  :config
+  :diminish 'omnisharp-mode
+  :init
   (progn
     ;; Note: Flycheck support needs manual fix
     ;; https://github.com/OmniSharp/omnisharp-emacs/issues/262
     (defun my-csharp ()
       (omnisharp-mode)
       (add-to-list 'company-backends 'company-omnisharp))
+    (add-hook 'csharp-mode-hook 'my-csharp t))
+  :config
+  (progn
+    (evil-leader/set-key-for-mode 'csharp-mode
+      "cf" 'omnisharp-code-format
+      "rj" 'omnisharp-go-to-definition
+      "rv" 'omnisharp-find-implementations
+      "rr" 'omnisharp-helm-find-usages
+      "rF" 'omnisharp-fix-code-issue-at-point)))
 
-    (after 'evil-leader
-      (evil-leader/set-key-for-mode 'csharp-mode
-        "cf" 'omnisharp-code-format
-        "rj" 'omnisharp-go-to-definition
-        "rv" 'omnisharp-find-implementations
-        "rr" 'omnisharp-helm-find-usages
-        "rF" 'omnisharp-fix-code-issue-at-point))
-
-    (add-hook 'csharp-mode-hook 'my-csharp)))
 
 (provide 'my-csharp)

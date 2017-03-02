efe3003396885c045f61c4721c3de6d6471ba537
Andres Senac <andres@senac.es>
2017-03-02 13:58:16 +0000

    Disable rtags for bison-mode

--- config/my-rtags.el
+++ config/my-rtags.el
@@ -75,7 +75,8 @@
 
     (defun my-c-mode-hook ()
       ;; my customizations for all of c-mode and related modes
-      (flycheck-select-checker 'rtags)
+      (if (not (string-equal major-mode "bison-mode"))
+          (flycheck-select-checker 'rtags))
       (local-set-key [s-mouse-1] 'rtags-find-symbol-at-point))
     (add-hook 'c-mode-hook 'my-c-mode-hook)
     (add-hook 'c++-mode-hook 'my-c-mode-hook)

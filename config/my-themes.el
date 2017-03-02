664af7021181af48c1d025c4c2d4bb42d3ad6e95
Andres Senac <andres@senac.es>
2017-03-02 05:30:32 +0000

    Themes

--- config/my-themes.el
+++ config/my-themes.el
@@ -1,9 +1,15 @@
-(use-package monokai-theme)
-;; (use-package gruvbox-theme)
-;; (use-package moe-theme)
+(use-package monokai-theme
+  :disabled t
+  :demand
+  :if (display-graphic-p)
+  :config (load-theme 'monokai t))
 
-(if (display-graphic-p)
-    (load-theme 'monokai t)
-  (load-theme 'tango-dark t))
+(use-package zenburn-theme
+  :demand
+  :if (display-graphic-p)
+  :config (load-theme 'zenburn t))
+
+(if (not (display-graphic-p))
+    (load-theme 'tango-dark t))
 
 (provide 'my-themes)

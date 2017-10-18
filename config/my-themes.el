2423487f0553380fcc3d048fd5063d19fbd23fdf
Andres Senac <andres@senac.es>
2017-10-18 08:35:32 +0200

    Try this theme out

--- config/my-themes.el
+++ config/my-themes.el
@@ -5,9 +5,16 @@
   :config (load-theme 'monokai t))
 
 (use-package zenburn-theme
+  :disabled t
   :demand
   :if (display-graphic-p)
   :config (load-theme 'zenburn t))
+
+(use-package atom-one-dark-theme
+  :demand
+  :if (display-graphic-p)
+  :config (load-theme 'atom-one-dark t))
+
 
 (if (not (display-graphic-p))
     (load-theme 'tango-dark t))

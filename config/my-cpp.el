09fbdea9137a7062ecab1f20367a1a7faf75dcf5
Andres Senac <asenac@nuodb.com>
2018-04-23 02:32:10 +0200

    Use company-lsp

--- config/my-cpp.el
+++ config/my-cpp.el
@@ -1,14 +1,18 @@
 (defun cquery//enable ()
-    (condition-case nil
-        (lsp-cquery-enable)
+  (condition-case nil
+      (lsp-cquery-enable)
     (user-error nil)))
+
+(defun my/company-lsp-hook ()
+  (add-to-list 'company-backends 'company-lsp))
 
 (use-package lsp-mode)
 (use-package lsp-ui
   :config
   (progn
     (add-hook 'lsp-mode-hook 'lsp-ui-mode)))
-(use-package company-lsp)
+(use-package company-lsp
+  :init (add-hook 'c-mode-common-hook #'my/company-lsp-hook))
 (use-package cquery
   :init (add-hook 'c-mode-common-hook #'cquery//enable)
   :commands lsp-cquery-enable

298db90870346926b4bb32513c35398a3aa27151
Andres Senac <asenac@nuodb.com>
2018-07-30 09:16:01 +0100

    Colorized compilation buffer

--- config/my-themes.el
+++ config/my-themes.el
@@ -15,6 +15,12 @@
   :if (display-graphic-p)
   :config (load-theme 'atom-one-dark t))
 
+(require 'ansi-color)
+(defun colorize-compilation-buffer ()
+  (toggle-read-only)
+  (ansi-color-apply-on-region compilation-filter-start (point))
+  (toggle-read-only))
+(add-hook 'compilation-filter-hook 'colorize-compilation-buffer)
 
 (if (not (display-graphic-p))
     (load-theme 'tango-dark t))

bc952ad3539c79a61f3231d382792cce9f8b38d1
Andres Senac <andres@senac.es>
2021-01-22 06:35:41 +0100

    Add LSP config

--- config/my-coding.el
+++ config/my-coding.el
@@ -134,3 +134,94 @@
         (add-hook 'python-mode-hook 'my/python-mode-hook)))))
 
 (provide 'my-coding)
+
+;;------------------------------------------------------------------------------
+;; lsp
+;;------------------------------------------------------------------------------
+(use-package lsp-mode
+  :ensure t
+  :config
+  (setq lsp-auto-guess-root t
+        lsp-prefer-flymake nil
+        ;; Kill the server when the last buffer in the workspace is deleted.
+        lsp-keep-workspace-alive nil
+        lsp-enable-file-watchers nil)
+
+  ;; We need to use this instead of c-mode-common-hook etc. so it can
+  ;; obey file-local and directory-local variable settings.
+  (add-hook 'hack-local-variables-hook
+            (lambda () (when (derived-mode-p 'c-mode 'c++-mode) (lsp)))))
+
+(use-package lsp-ui
+  :ensure t)
+
+(use-package ccls
+  :ensure t
+  :after lsp-mode
+  :config
+  ;; Enable for debugging
+  (setq ccls-args '("--log-file=/tmp/ccls.out" "-v=1"))
+
+  (defun ccls/callee () (interactive) (lsp-ui-peek-find-custom "$ccls/call" '(:callee t)))
+  (defun ccls/caller () (interactive) (lsp-ui-peek-find-custom "$ccls/call"))
+
+  (defun ccls/base ()
+    (interactive)
+    (lsp-ui-peek-find-custom "$ccls/inheritance"))
+  (defun ccls/derived ()
+    (interactive)
+    (lsp-ui-peek-find-custom "$ccls/inheritance" `(:derived t)))
+
+  (defun ccls/vars ()
+    (interactive)
+    (lsp-ui-peek-find-custom "$ccls/vars"))
+  (defun ccls/member ()
+    (interactive)
+    (lsp-ui-peek-find-custom "$ccls/member"))
+
+  ;; References w/ Role::Role
+  (defun ccls/references-read ()
+    (interactive)
+    (lsp-ui-peek-find-custom "textDocument/references"
+                             (plist-put (lsp--text-document-position-params) :role 8)))
+
+  ;; References w/ Role::Write
+  (defun ccls/references-write ()
+    (interactive)
+    (lsp-ui-peek-find-custom "textDocument/references"
+                             (plist-put (lsp--text-document-position-params) :role 16)))
+
+  ;; References w/ Role::Dynamic bit (macro expansions)
+  (defun ccls/references-macro ()
+    (interactive)
+    (lsp-ui-peek-find-custom "textDocument/references"
+                             (plist-put (lsp--text-document-position-params) :role 64)))
+
+  ;; References w/o Role::Call bit (e.g. where functions are taken addresses)
+  (defun ccls/references-not-call ()
+    (interactive)
+    (lsp-ui-peek-find-custom "textDocument/references"
+                             (plist-put (lsp--text-document-position-params) :excludeRole 32)))
+
+  (after 'evil-leader
+    (evil-leader/set-key-for-mode 'c-mode
+      "lc" 'ccls/caller
+      "lj" 'lsp-find-definition
+      "lr" 'lsp-find-references)
+    (evil-leader/set-key-for-mode 'c++-mode
+      "lc" 'ccls/caller
+      "lj" 'lsp-find-definition
+      "lr" 'lsp-find-references))
+)
+
+(use-package helm-xref
+  :config
+  (setq xref-show-xrefs-function 'helm-xref-show-xrefs))
+
+(use-package company-lsp
+  :ensure t
+  ;; Allow ccls to handle completion
+  :config
+  (setq company-transformers nil
+        company-lsp-async t
+        company-lsp-cache-candidates nil))

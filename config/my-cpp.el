f725df6e2a8663c9a94aec81b4f082fddd436f7d
Andres Senac <andres@senac.es>
2021-01-22 06:18:15 +0100

    Disable rtags, rearrange coding-related packages

--- config/my-cpp.el
+++ config/my-cpp.el
@@ -1,11 +1,96 @@
-(defun my-c-mode-hook ()
-    ;; my customizations for all of c-mode and related modes
-    (if (not (string-equal major-mode "bison-mode"))
-        (flycheck-select-checker 'rtags))
-    (local-set-key [s-mouse-1] 'rtags-find-symbol-at-point))
-(if (not (string-equal system-type "windows-nt"))
-    (progn
-      (add-hook 'c-mode-hook 'my-c-mode-hook)
-      (add-hook 'c++-mode-hook 'my-c-mode-hook)))
+;;------------------------------------------------------------------------------
+;; clang-format
+;;------------------------------------------------------------------------------
+(use-package clang-format
+  :commands clang-format)
+
+;;------------------------------------------------------------------------------
+;; rtags
+;;------------------------------------------------------------------------------
+(defvar rtags-path "~/local/rtags/share/emacs/site-lisp/rtags")
+(add-to-list 'load-path rtags-path)
+(use-package rtags
+  :disabled t
+  :if (file-directory-p rtags-path)
+  :unless (string-equal system-type "windows-nt")
+  :ensure nil
+  :config
+  (progn
+    (require 'helm-rtags)
+    (require 'flycheck-rtags)
+    (setq rtags-jump-to-first-match nil)
+    (setq rtags-display-result-backend 'helm)
+    (setq rtags-use-bookmarks nil)
+    (rtags-enable-standard-keybindings)
+
+    (after 'evil
+      (define-key evil-motion-state-map "[r" 'rtags-previous-match)
+      (define-key evil-motion-state-map "]r" 'rtags-next-match)
+      (evil-set-command-property 'rtags-find-symbol-at-point :jump t)
+      (evil-set-command-property 'rtags-find-all-references-at-point :jump t)
+      (evil-set-command-property 'rtags-find-symbol :jump t)
+      (evil-set-command-property 'rtags-find-virtuals-at-point :jump t)
+
+      ;; (add-to-list 'evil-emacs-state-modes 'rtags-mode)
+      (evil-set-initial-state 'rtags-mode 'normal)
+      (evil-define-key 'normal rtags-mode-map
+        (kbd "RET") 'rtags-select-and-remove-rtags-buffer
+        (kbd "SPC") 'rtags-select-other-window)
+      )
+
+    (defun my-c-mode-hook ()
+      ;; my customizations for all of c-mode and related modes
+      (if (not (string-equal major-mode "bison-mode"))
+          (flycheck-select-checker 'rtags))
+      (local-set-key [s-mouse-1] 'rtags-find-symbol-at-point))
+    (if (not (string-equal system-type "windows-nt"))
+        (progn
+          (add-hook 'c-mode-hook 'my-c-mode-hook)
+          (add-hook 'c++-mode-hook 'my-c-mode-hook)))
+
+    (after 'hydra
+      (defhydra hydra-rtags (:color pink :hint nil :exit t)
+        "
+
+[_j_]: symbol at point      [_s_]: find symbol    [_p_]: start process
+[_v_]: virtuals at point    [_f_]: find file      [_l_]: buffer
+[_r_]: refs at point        [_F_]: fixit
+[_m_]: summary              [_i_]: imenu
+
+"
+        ("j" rtags-find-symbol-at-point)
+        ("v" rtags-find-virtuals-at-point)
+        ("r" rtags-find-all-references-at-point)
+        ("m" rtags-display-summary)
+        ("i" rtags-imenu)
+        ("p" rtags-start-process-unless-running)
+        ("l" rtags-show-rtags-buffer)
+        ("s" rtags-find-symbol)
+        ("f" rtags-find-file)
+        ("F" rtags-fixit)
+        ("q" nil "cancel"))
+      (after 'evil-leader
+        (evil-leader/set-key-for-mode 'c-mode
+          "rj" 'rtags-find-symbol-at-point
+          "rr" 'rtags-find-all-references-at-point
+          "rv" 'rtags-find-virtuals-at-point
+          "rm" 'rtags-display-summary
+          "i"  'rtags-imenu)
+        (evil-leader/set-key-for-mode 'c++-mode
+          "rj" 'rtags-find-symbol-at-point
+          "rr" 'rtags-find-all-references-at-point
+          "rv" 'rtags-find-virtuals-at-point
+          "rm" 'rtags-display-summary
+          "i"  'rtags-imenu)
+        (evil-leader/set-key
+          ",r" 'hydra-rtags/body
+          "rp" 'rtags-start-process-unless-running
+          "rl" 'rtags-show-rtags-buffer
+          "rs" 'rtags-find-symbol
+          "rf" 'rtags-find-file
+          "rF" 'rtags-fixit
+          "rn" 'rtags-location-stack-forward
+          "rb" 'rtags-location-stack-backward)))
+    ))
 
 (provide 'my-cpp)
--- config/my-pkgs.el
+++ config/my-pkgs.el
@@ -31,109 +31,10 @@
 (add-hook 'c-mode-common-hook #'display-line-numbers-mode)
 
 ;;------------------------------------------------------------------------------
-;; yasnippet
-;;------------------------------------------------------------------------------
-(use-package yasnippet
-  :diminish 'yas-minor-mode
-  :defer 2
-  :config
-  (progn
-    ;; (yas-global-mode 1)
-
-    (after 'evil
-      ;; Instead of enabling yasnippet globally I think it's better to enable it
-      ;; when entering insert mode
-      (add-hook
-       'evil-insert-state-entry-hook
-       (lambda ()
-         (if (string-equal major-mode "term-mode")
-             (yas-minor-mode -1)
-           (yas-minor-mode 1))))
-
-      (add-hook
-       'evil-insert-state-exit-hook
-       (lambda ()
-         (yas-minor-mode -1))))
-
-    (setq my-yasnippets (expand-file-name "~/.emacs.d/snippets"))
-    (if (and (file-exists-p my-yasnippets)
-             (not (member my-yasnippets yas-snippet-dirs)))
-        (add-to-list 'yas-snippet-dirs my-yasnippets))
-
-    (yas-reload-all)
-    ))
-
-;;------------------------------------------------------------------------------
-;; company
-;;------------------------------------------------------------------------------
-(use-package company
-  :diminish 'company-mode
-  :config
-  (progn
-    (add-hook 'after-init-hook 'global-company-mode)
-    (after 'evil
-        (define-key evil-insert-state-map (kbd "<C-return>") 'company-complete))
-
-    (use-package company-jedi
-      ;; Disabled, completion provided by ycmd
-      :disabled t
-      :config
-      (progn
-        (defun my/python-mode-hook ()
-          (add-to-list 'company-backends 'company-jedi))
-        (add-hook 'python-mode-hook 'my/python-mode-hook)))))
-
-;;------------------------------------------------------------------------------
-;; flycheck
-;;------------------------------------------------------------------------------
-(use-package flycheck
-  ;; :diminish 'flycheck-mode
-  :config
-  (progn
-    (setq flycheck-mode-line-prefix "F")
-
-    (after 'evil
-      (define-key evil-motion-state-map "[e" 'flycheck-previous-error)
-      (define-key evil-motion-state-map "]e" 'flycheck-next-error))
-
-    (add-hook 'after-init-hook 'global-flycheck-mode)))
-
-;;------------------------------------------------------------------------------
 ;; flyspell
 ;;------------------------------------------------------------------------------
 (use-package flyspell
   :diminish 'flyspell-prog-mode)
-
-;;------------------------------------------------------------------------------
-;; ycmd
-;;------------------------------------------------------------------------------
-(use-package ycmd
-  :unless (string-equal system-type "windows-nt")
-  :diminish 'ycmd-mode
-  :config
-  (progn
-    (let ((option-found (my/return-first-file-found
-                         ["~/.emacs.d/static/ycmd/ycmd"
-                         "~/.vim/bundle/YouCompleteMe/third_party/ycmd/ycmd"])))
-      (if option-found
-          (progn
-            (message (concat "Ycmd found at: " option-found))
-            (set-variable 'ycmd-server-command '("python"))
-            (add-to-list
-             'ycmd-server-command
-             (expand-file-name option-found) t)
-            (set-variable 'ycmd-extra-conf-whitelist '("~/*" "/*"))
-            (ycmd-setup)
-
-            (after 'company
-              (use-package company-ycmd)
-              (company-ycmd-setup))
-
-            (after 'flycheck
-              (use-package flycheck-ycmd
-                :config (flycheck-ycmd-setup))))
-        ; Not found
-        (message "Ycmd not found!")))))
 
 ;;------------------------------------------------------------------------------
 ;; recentf
@@ -162,12 +63,6 @@
 ;;------------------------------------------------------------------------------
 (use-package minimap
   :commands minimap-mode)
-
-;;------------------------------------------------------------------------------
-;; clang-format
-;;------------------------------------------------------------------------------
-(use-package clang-format
-  :commands clang-format)
 
 ;;------------------------------------------------------------------------------
 ;; fiplr
@@ -214,38 +109,6 @@
   :config
   (progn
     (use-package org-bullets)))
-
-;;------------------------------------------------------------------------------
-;; eopengrok
-;;------------------------------------------------------------------------------
-(use-package eopengrok
-  :unless (string-equal system-type "windows-nt")
-  :commands (eopengrok-make-index
-             eopengrok-find-definition
-             eopengrok-find-file
-             eopengrok-find-reference
-             eopengrok-find-text
-             eopengrok-find-history
-             eopengrok-switch-to-buffer
-             eopengrok-jump-to-source)
-  :init
-  (progn
-    (after 'evil-leader
-      (evil-leader/set-key
-        "ogi" 'eopengrok-make-index
-        "ogd" 'eopengrok-find-definition
-        "ogf" 'eopengrok-find-file
-        "ogr" 'eopengrok-find-reference
-        "ogt" 'eopengrok-find-text
-        "ogh" 'eopengrok-find-history
-        "ogb" 'eopengrok-switch-to-buffer
-        "ogj" 'eopengrok-jump-to-source)))
-  :config
-  (progn
-    (setq eopengrok-jar (expand-file-name "~/local/clj-opengrok-0.3.0-standalone.jar"))
-    (if (string-equal system-type "darwin") ; Mac OS X
-        (setq eopengrok-ctags "/usr/local/Cellar/ctags/5.8/bin/ctags")
-      (setq eopengrok-ctags "/usr/bin/ctags"))))
 
 ;;------------------------------------------------------------------------------
 ;; telephone-line
